1afeefcaf8e0b299c2e71edb8c07616331ea2077
sonjatiago <sonjatiago@outlook.com>
2025-01-18 19:20:10 +0200

    sss

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -24,6 +24,7 @@ const Partners = () => {
     <div className="partners-container">
       <motion.h2
         initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
       >
         {t.ourPartners}
@@ -48,10 +49,19 @@ const Partners = () => {
             <motion.div
               key={`${partner.id}-${index}`}
               className="partner-logo"
-                          whileHover={{ opacity: 1, scale: 1.05 }}
-              transition={{ duration: 0.3 }}
+              whileHover={{ 
+                scale: 1.05,
+                transition: { duration: 0.3 }
+              }}
+              whileTap={{ 
+                scale: 0.98 
+              }}
             >
-              <img src={partner.logo} alt={partner.name} />
+              <img 
+                src={partner.logo} 
+                alt={partner.name}
+                loading="lazy"
+              />
             </motion.div>
           ))}
         </motion.div>
